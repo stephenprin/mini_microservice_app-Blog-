2cbe227604ff993a39e40e9870c83e64c08be42f
stephenprin <stephenprince427@gmail.com>
2023-03-16 15:44:11 +0100

    added the logic to add comment

---
+++ comments/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comment', (req, res) => { 
+   res.status(200).send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comment', (req, res) => { 
+    const commentId = randomBytes(8).toString('hex');
+    const { content } = req.body; 
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+    
+});
+
+app.listen(4001, () => { 
+    console.log('Listening on 4001');
+})
